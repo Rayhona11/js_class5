59a7a746fa52120207a3b2462fff65cca46b638d
Rayhona <rayhonamirzoyeva.ru>
2024-02-08 13:58:35 +0500

    dxgjf

---
+++ script2.js
@@ -0,0 +1,6 @@
+document.querySelector('#on').onclick = ()=>{
+    document.querySelector('.prop').classList.add('rotate')
+}
+document.querySelector('#off').onclick = ()=>{
+    document.querySelector('.prop').classList.remove('rotate')
+}
